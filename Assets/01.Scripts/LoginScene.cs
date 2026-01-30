61abf650575a874eb76e478906cc216d84ddccc3
mongilteacher <mongilteacher@gmail.com>
2026-01-30 13:14:30 +0900

    feat: 로그인씬

---
+++ Assets/01.Scripts/LoginScene.cs
@@ -0,0 +1,145 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoginScene : MonoBehaviour
+{
+    // 로그인씬 (로그인/회원가입) -> 게임씬
+
+    private enum SceneMode
+    {
+        Login,
+        Register
+    }
+    
+    private SceneMode _mode = SceneMode.Login;
+    
+    // 비밀번호 확인 오브젝트
+    [SerializeField] private GameObject _passwordCofirmObject;
+    [SerializeField] private Button _gotoRegisterButton;
+    [SerializeField] private Button _loginButton;
+    [SerializeField] private Button _gotoLoginButton;
+    [SerializeField] private Button _registerButton;
+
+    [SerializeField] private TextMeshProUGUI _messageTextUI;
+    
+    [SerializeField] private TMP_InputField _idInputField;
+    [SerializeField] private TMP_InputField _passwordInputField;
+    [SerializeField] private TMP_InputField _passwordConfirmInputField;
+    
+    private void Start()
+    {
+        AddButtonEvents();
+        Refresh();
+    }
+
+    private void AddButtonEvents()
+    {
+        _gotoRegisterButton.onClick.AddListener(GotoRegister);
+        _loginButton.onClick.AddListener(Login);
+        _gotoLoginButton.onClick.AddListener(GotoLogin);
+        _registerButton.onClick.AddListener(Register);
+    }
+
+    private void Refresh()
+    {
+        // 2차 비밀번호 오브젝트는 회원가입 모드일때만 노출
+        _passwordCofirmObject.SetActive(_mode == SceneMode.Register);
+        _gotoRegisterButton.gameObject.SetActive(_mode == SceneMode.Login);
+        _loginButton.gameObject.SetActive(_mode == SceneMode.Login);
+        _gotoLoginButton.gameObject.SetActive(_mode == SceneMode.Register);
+        _registerButton.gameObject.SetActive(_mode == SceneMode.Register);
+    }
+
+    private void Login()
+    {
+        // 로그인
+        // 1. 아이디 입력을 확인한다.
+        string id = _idInputField.text;
+        if (string.IsNullOrEmpty(id))
+        {
+            _messageTextUI.text = "아이디를 입력해주세요.";
+            return;
+        }
+        
+        // 2. 비밀번호 입력을 확인한다.
+        string password = _passwordInputField.text;
+        if (string.IsNullOrEmpty(password))
+        {
+            _messageTextUI.text = "패스워드를 입력해주세요.";
+            return;
+        }
+        
+        // 3. 실제 저장된 아이디-비밀번호 계정이 있는지 확인한다.
+        // 3-1. 아이디가 있는지 확인한다.
+        if (!PlayerPrefs.HasKey(id))
+        {
+            _messageTextUI.text = "아이디/비밀번호를 확인해주세요.";
+            return;
+        }
+        
+        string myPassword = PlayerPrefs.GetString(id);
+        if (myPassword != password)
+        {
+            _messageTextUI.text = "아이디/비밀번호를 확인해주세요.";
+            return;
+        }
+        
+        // 4. 있다면 씬 이동
+        SceneManager.LoadScene("LoadingScene");
+    }
+
+    private void Register()
+    {
+        // 로그인
+        // 1. 아이디 입력을 확인한다.
+        string id = _idInputField.text;
+        if (string.IsNullOrEmpty(id))
+        {
+            _messageTextUI.text = "아이디를 입력해주세요.";
+            return;
+        }
+        
+        // 2. 비밀번호 입력을 확인한다.
+        string password = _passwordInputField.text;
+        if (string.IsNullOrEmpty(password))
+        {
+            _messageTextUI.text = "패스워드를 입력해주세요.";
+            return;
+        }
+        
+        // 2. 2ck 비밀번호 입력을 확인한다.
+        string password2 = _passwordInputField.text;
+        if (string.IsNullOrEmpty(password2) || password != password2)
+        {
+            _messageTextUI.text = "패스워드를 확인해주세요.";
+            return;
+        }
+        
+        // 4. 실제 저장된 아이디-비밀번호 계정이 있는지 확인한다.
+        // 4-1. 아이디가 있는지 확인한다.
+        if (PlayerPrefs.HasKey(id))
+        {
+            _messageTextUI.text = "중복된 아이디입니다.";
+            return;
+        }
+
+        PlayerPrefs.SetString(id, password);
+
+        GotoLogin();
+    }
+
+    private void GotoLogin()
+    {
+        _mode = SceneMode.Login;
+        Refresh();
+    }
+
+    private void GotoRegister()
+    {
+        _mode = SceneMode.Register;
+        Refresh();
+    }
+    
+}
